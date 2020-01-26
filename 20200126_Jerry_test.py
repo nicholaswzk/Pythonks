bef339cba30bda7de5f9b931c52e0ea0097a247f
Jerry <frezzey123@gmail.com>
2020-01-26 19:06:03 +0800

    20200126_Update
    Created and modified 20200126_Jerry_test.py

---
+++ 20200126_Jerry_test.py
@@ -0,0 +1,21 @@
+import numpy as np
+import osmnx as ox
+import networkx as nx
+
+t = (1.3984, 103.9072)
+G = ox.graph_from_point(t, distance=1200)
+
+#ox.plot_graph(G, fig_height=10, fig_width=10, edge_color="black")
+
+print(np.random.choice(G.nodes))
+
+kk = np.random.choice(G.nodes)
+print(G.nodes[kk]['y'][1])
+#xx = (G.nodes[kk]['y'], G.nodes[kk]['x'])
+
+#print(ox.get_node(xx))
+
+#route = nx.shortest_path(G, np.random.choice(G.nodes), np.random.choice(G.nodes))
+
+
+#ox.plot_graph_route(G, route, fig_height=10, fig_width=10)
