8846d131d55eded96ad8cae7570f4b5f95c0f64c
Jerry <frezzey123@gmail.com>
2020-01-27 17:01:58 +0800

    20200127_Update_Jerry
    Modified 20200126_Jerry_test.py, testedd some codes

--- 20200126_Jerry_test.py
+++ 20200126_Jerry_test.py
@@ -5,15 +5,36 @@ import networkx as nx
 t = (1.3984, 103.9072)
 G = ox.graph_from_point(t, distance=1200)
 
+o = list(G.nodes())[0]
+d = list(G.nodes())[20]
+
+nextlevel={o:1}
+
+paths={o:[o]}
+
+print(nextlevel)
+
+print(G[o])
+
+for v in nextlevel:
+    print(v, " test")
+    for w in G[v]:
+        print(w, " test2")
+
+print(paths)
+
+print(o, d)
+
+
 n, e = ox.graph_to_gdfs(G)
 
 print(n.head())
 
 print(e.head())
 
-n.to_csv("nodes.csv")
+#n.to_csv("nodes.csv")
 
-e.to_csv("e.csv")
+#e.to_csv("e.csv")
 
 #ox.plot_graph(G, fig_height=10, fig_width=10, edge_color="black")
 
