fc811a3322c3ebc51ba59d3703b8c79410d07df9
Jerry <frezzey123@gmail.com>
2020-03-04 15:29:34 +0800

    20200304_Update_Jerry
    Random_Testing

--- 20200228_Jerry_function.py
+++ 20200228_Jerry_function.py
@@ -7,20 +7,118 @@ import pandas as pd
 import MyQueue
 import itertools as it
 import geopandas as gpd
+from toolbox import connect_poi
+from geopandas.geodataframe import GeoDataFrame
+from geopy.point import Point
+
+
+
+# import re
+# import time
+# import os
+# import ast
+# import numpy as np
+# import pandas as pd
+# import geopandas as gpd
+# import networkx as nx
+# from shapely.geometry import Point
+# from shapely.geometry import LineString
+# from shapely import wkt
+# from xml.etree import ElementTree as etree
 
 #matplotlib inline
 
 old_market = (1.4052585, 103.9023302)
 
-box = (1.4220, 1.3863, 103.9259, 103.8895)
+#box = (1.4220, 1.3863, 103.9259, 103.8895)
 
-G = ox.create_poi_gdf("map.geojson")
+#G = ox.create_poi_gdf("map.geojson")
 
 #G = ox.graph_from_file("map")
 
+pois = gpd.read_file("busstop.geojson")
+
+pois['key'] = pois.index
+
+print(pois.head(5))
+
+nodes = gpd.read_file('data/sample/nodes/nodes.shp')
+edges = gpd.read_file('data/sample/edges/edges.shp')
+
+new_nodes, new_edges = connect_poi(pois, nodes, edges, key_col='key', path=None)
+
+    
+
+#ox.plot_graph(G2, fig_height=10, fig_width=10, edge_color="black")
+
 #G = ox.graph_from_bbox(1.42103880000, 1.39289490000, 103.92311100000, 103.89616010000, simplify=True, retain_all=False)# quick plot
 
-#G = ox.graph_from_point(old_market, distance=2500, truncate_by_edge=True)# quick plot
+# G = ox.graph_from_point(old_market, distance=2500, truncate_by_edge=True)# quick plot
+
+# node, edge = ox.graph_to_gdfs(G)
+
+
+
+
+def gdfs_to_grapht(gdf_nodes, gdf_edges):
+    """
+    Convert node and edge GeoDataFrames into a graph
+    Parameters
+    ----------
+    gdf_nodes : GeoDataFrame
+    gdf_edges : GeoDataFrame
+    Returns
+    -------
+    networkx multidigraph
+    """
+
+    G = nx.MultiDiGraph()
+    G.graph['crs'] = gdf_nodes.crs
+    print(gdf_nodes.crs)
+    #print(gdf_nodes.gdf_name)
+    G.graph['name'] = "unnamed"
+
+    # add the nodes and their attributes to the graph
+    G.add_nodes_from(gdf_nodes.index)
+    attributes = gdf_nodes.to_dict()
+    for attribute_name in gdf_nodes.columns:
+        # only add this attribute to nodes which have a non-null value for it
+        attribute_values = {k:v for k, v in attributes[attribute_name].items() if pd.notnull(v)}
+        nx.set_node_attributes(G, name=attribute_name, values=attribute_values)
+
+    # add the edges and attributes that are not u, v, key (as they're added
+    # separately) or null
+    for _, row in gdf_edges.iterrows():
+        attrs = {}
+        for label, value in row.iteritems():
+            if (label not in ['from', 'to', 'key']) and (isinstance(value, list) or pd.notnull(value)):
+                attrs[label] = value
+        G.add_edge(row['from'], row['to'], key=row['key'], **attrs)
+
+    return G
+
+#G2 = gdfs_to_grapht(node, edge)
+
+G2 = gdfs_to_grapht(new_nodes,new_edges)
+
+ttn,tte = ox.graph_to_gdfs(G2)
+
+ttn.to_csv("testt2.csv")
+# delete all rows with column 'Age' has value 30 to 40 
+indexNames = ttn[ (ttn['x'] == 'None') ].index
+ttn.drop(indexNames , inplace=True)
+
+ttn.to_csv("testttttt.csv")
+
+# ttn = gpd.read_file("ttttn.csv")
+
+# tte = gpd.read_file("tttte.csv")
+
+G3 = ox.gdfs_to_graph(ttn,tte)
+
+# ttn.to_csv("ttttn.csv")
+# tte.to_csv("tttte.csv")
+ox.plot_graph(G3, fig_height=10, fig_width=10, edge_color="black")
 
 def heuristic(curnode, endnode):
    return math.sqrt((G.nodes[endnode].get('x') - G.nodes[curnode].get('x')) ** 2 + (G.nodes[endnode].get('y') - G.nodes[curnode].get('y')) ** 2)
@@ -69,8 +167,8 @@ def astar(startN, endN):
         #? {'osmid': 35091912, 'oneway': True, 'lanes': '2', 'name': 'Punggol Place', 'highway': 'residential', 'maxspeed': '40', 'length': 25.879} 1
         #! ndis is new distance
         for nei, etc in G[curN].items():
-            #TODO(DEBUG):  print(nei, etc)
-            #TODO(DEBUG):   print(curN, nei)
+            # TODO print(nei, etc)
+            # TODO print(curN, nei)
             if nei in used:
                 continue
             #! 'length' can be any numeric data in etc
@@ -79,16 +177,41 @@ def astar(startN, endN):
             pq.push(f,next(c), nei, ndis, curN)
     return -1
 
+# pois = gpd.read_file("busstop.geojson")
 
-n, e = ox.graph_to_gdfs(G)
+# print(pois.head())
 
-print(n.head())
+# nodes = gpd.read_file('data/sample/nodes/nodes.shp')
+# edges = gpd.read_file('data/sample/edges/edges.shp')
 
-print(e.head())
+# new_nodes, new_edges = connect_poi(pois, nodes, edges, key_col='id', path=None)
 
-n.to_csv("nodes.csv")
+# poi_links = new_edges[new_edges['highway'] == 'poi']
+# ax = edges.plot(linewidth=0.8, figsize=(18,10), label='Original Road Edges')
+# poi_links.plot(color='indianred', linewidth=2, ax=ax, label='New Connection Edges')
+# pois.plot(color='indianred', marker='.', markersize=200, ax=ax, label='POI')
+# ax.legend(loc=2, fontsize=18)
+# ax.set_title('The integrated network of supermarkets and road network at Toa Payoh', fontsize=22)
 
-e.to_csv("e.csv")
+# new_nodes.to_csv("n1.csv")
+# new_edges.to_csv("e1.csv")
+#new_nodes, new_edges = connect_poi(pois, nodes, edges, key_col='key', path=None)
+
+
+#ox.plot_graph(G, fig_height=10, fig_width=10, edge_color="black")
+
+#! ---------------------------Testing---------------------------
+# n, e = ox.graph_to_gdfs(G)
+
+#ox.save_graph_shapefile(G, filename='sample', folder=None, encoding='utf-8')
+
+# print(n.head())
+
+# print(e.head())
+
+# n.to_csv("nodes.csv")
+
+# e.to_csv("e.csv")
 
 # countries_gdf = gpd.read_file("punggol.geojson")
 
@@ -103,5 +226,7 @@ e.to_csv("e.csv")
 # t2 = ox.get_nearest_node(G, test2xy)
 
 # print(astar(t1,t2))
+
+#! ---------------------------Testing---------------------------
 
 
